a762fe40b64882c896a2d49cdcb47ff0a1bf5c6c
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2021-10-17 20:15:00 -0300

    Adicionando função de scroll no menu

--- main.js
+++ main.js
@@ -17,3 +17,16 @@ for(const l of links){
         nav.classList.remove('show');
     })
 }
+
+// MUDAR O HEADER DA PÁGINA QUANDO DE SCROLL
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight;
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY >= navHeight){
+        header.classList.add('scroll')
+    }else{
+        header.classList.remove('scroll')
+    }
+})
