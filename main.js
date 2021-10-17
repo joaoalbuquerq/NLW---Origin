6183b1a923a87ad52c3545794e31be604f1aeb3d
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2021-10-17 18:00:58 -0300

    Adicionando funcionalidade de abrir e fehcar o menu e de quando clicar em um link, fechar o menu

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-alert('salve compacinhas')
+//Funcionalidade de abrir e fechar o menu
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle');
+
+for(const element of toggle){
+    element.addEventListener('click', function(){
+        nav.classList.toggle('show');
+    })
+}
+
+
+//Esconde o menu quando clicar em um link do menu
+const links = document.querySelectorAll('nav ul li a')
+
+for(const l of links){
+    l.addEventListener('click', function(){
+        nav.classList.remove('show');
+    })
+}
