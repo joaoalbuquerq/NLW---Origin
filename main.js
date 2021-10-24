617b1e3583e286e37a62abc8f6644d6cefb1605f
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2021-10-24 11:06:02 -0300

    Adicionando seção de slide de testemunhos

--- main.js
+++ main.js
@@ -30,3 +30,15 @@ window.addEventListener('scroll', function(){
         header.classList.remove('scroll')
     }
 })
+
+
+// CARROSSEL DE TESTEMUNHOS
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true
+  })
